bc8b86375f67c39e19545a12346a08a5069256c6
David Mateita <david.mateita@gmail.com>
2025-04-15 22:50:46 +0300

    feat(tests): configure Unit tests,  use LazilyRefreshDatabase instead
    fix: vite error in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,17 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Config;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite(); // Fake Vite for tests - for GH Actions
+
+        Config::set('filesystems.disks.public.root', storage_path('app/public_test'));
+        Config::set('filesystems.disks.local.root', storage_path('app/private_test'));
+    }
 }
